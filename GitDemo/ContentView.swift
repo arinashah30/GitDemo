777c83f8dedcba88625dd687676a059d8b12443b
arinashah30 <ashah634@gatech.edu>
2024-02-06 16:51:20 -0500

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by Arina Shah on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Arina Shah on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
