6f1c86223b13b74c8814a38265aeb286f9676c1f
Younis, Lana <lyounis6@gatech.edu>
2024-02-06 16:52:37 -0500

    Added Arina file

---
+++ GitDemo/Arina.swift
@@ -0,0 +1,8 @@
+//
+//  Arina.swift
+//  GitDemo
+//
+//  Created by Arina Shah on 2/6/24.
+//
+
+import Foundation
---
+++ GitDemo/lana.swift
@@ -0,0 +1,8 @@
+//
+//  lana.swift
+//  GitDemo
+//
+//  Created by lana on 2/6/24.
+//
+
+import Foundation
